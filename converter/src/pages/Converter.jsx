a2a63026ec2737d419fdcb1da2f3648cf71267a6
ViktorJun <viktorbylash@gmail.com>
2026-04-20 11:58:53 +0300

    create currency & history converter

--- converter/src/pages/Converter.jsx
+++ converter/src/pages/Converter.jsx
@@ -1,6 +1,13 @@
+import {AdvertisingBanner} from "../components/AdvertisingBanner.jsx";
+import {CurrencyConverter} from "../components/CurrencyConverter.jsx";
+import {HistoryConverter} from "../components/HistoryConverter.jsx";
 
 export function Converter() {
     return (
-        <div></div>
+        <div>
+            <AdvertisingBanner />
+            <CurrencyConverter />
+            <HistoryConverter />
+        </div>
     )
 }
